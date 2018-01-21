b6984e88f724a72850f258959ca77b558b0d51fc
Lehi Toskin <lehi@tosk.in>
2018-01-20 23:31:42 -0800

    Basic functionality

--- db.rkt
+++ db.rkt
@@ -1,9 +1,306 @@
 #lang racket/base
 ; db.rkt
-(require racquel db/base db/sqlite3 "files.rkt")
-(provide sqlc)
+(require racket/class
+         racket/contract
+         racket/string
+         racquel
+         db/base
+         db/sqlite3
+         "files.rkt")
+(provide db-has-key?
+         del-title!
+         get-comments
+         get-date-end
+         get-date-start
+         get-rating
+         get-progress
+         set-info!
+         sqlc
+         table-titles)
 
 (define sqlc
   (sqlite3-connect
    #:database db-path
    #:mode 'create))
+
+(define tables '("Anime"
+                 "Cartoon"
+                 "Comic"
+                 "LN"
+                 "Manga"
+                 "Television"))
+
+; make sure the table and columns exist
+(for ([medium (in-list tables)])
+  (define str
+    (format "create table if not exists ~a(Title string not null, Rating integer,
+             Date_Start string, Progress string, Date_End string, Comments string);" medium))
+  (query-exec sqlc str))
+
+(define anime%
+  (data-class object%
+              (table-name "Anime")
+              (init-column (title "" "Title"))
+              (column (rating 3 "Rating")
+                      (date-start "" "Date_Start")
+                      (progress "" "Progress")
+                      (date-end "" "Date_End")
+                      (comments "" "Comments"))
+              (primary-key title)
+              (super-new)
+
+              (define/public (set-info! r ds p de c)
+                (set! rating r)
+                (set! date-start ds)
+                (set! progress p)
+                (set! date-end de)
+                (set! comments c))))
+
+(define cartoon%
+  (data-class object%
+              (table-name "Cartoon")
+              (init-column (title "" "Title"))
+              (column (rating 3 "Rating")
+                      (date-start "" "Date_Start")
+                      (progress "" "Progress")
+                      (date-end "" "Date_End")
+                      (comments "" "Comments"))
+              (primary-key title)
+              (super-new)
+
+              (define/public (set-info! r ds p de c)
+                (set! rating r)
+                (set! date-start ds)
+                (set! progress p)
+                (set! date-end de)
+                (set! comments c))))
+
+(define comic%
+  (data-class object%
+              (table-name "Comic")
+              (init-column (title "" "Title"))
+              (column (rating 3 "Rating")
+                      (date-start "" "Date_Start")
+                      (progress "" "Progress")
+                      (date-end "" "Date_End")
+                      (comments "" "Comments"))
+              (primary-key title)
+              (super-new)
+
+              (define/public (set-info! r ds p de c)
+                (set! rating r)
+                (set! date-start ds)
+                (set! progress p)
+                (set! date-end de)
+                (set! comments c))))
+
+(define ln%
+  (data-class object%
+              (table-name "LN")
+              (init-column (title "" "Title"))
+              (column (rating 3 "Rating")
+                      (date-start "" "Date_Start")
+                      (progress "" "Progress")
+                      (date-end "" "Date_End")
+                      (comments "" "Comments"))
+              (primary-key title)
+              (super-new)
+
+              (define/public (set-info! r ds p de c)
+                (set! rating r)
+                (set! date-start ds)
+                (set! progress p)
+                (set! date-end de)
+                (set! comments c))))
+
+(define manga%
+  (data-class object%
+              (table-name "Manga")
+              (init-column (title "" "Title"))
+              (column (rating 3 "Rating")
+                      (date-start "" "Date_Start")
+                      (progress "" "Progress")
+                      (date-end "" "Date_End")
+                      (comments "" "Comments"))
+              (primary-key title)
+              (super-new)
+
+              (define/public (set-info! r ds p de c)
+                (set! rating r)
+                (set! date-start ds)
+                (set! progress p)
+                (set! date-end de)
+                (set! comments c))))
+
+(define television%
+  (data-class object%
+              (table-name "Television")
+              (init-column (title "" "Title"))
+              (column (rating 3 "Rating")
+                      (date-start "" "Date_Start")
+                      (progress "" "Progress")
+                      (date-end "" "Date_End")
+                      (comments "" "Comments"))
+              (primary-key title)
+              (super-new)
+
+              (define/public (set-info! r ds p de c)
+                (set! rating r)
+                (set! date-start ds)
+                (set! progress p)
+                (set! date-end de)
+                (set! comments c))))
+
+; scrub some inputs before adding to database
+; sql queries will complain for several reasons:
+; - if a tag has spaces, but no quotes around it
+; - if the tag contains quotes, so add single quotes around the entire thing
+; - if the tag contains ', so replace it with ''
+(define (input-scrub str)
+  (format "'~a'" (string-replace str "'" "''")))
+
+(define/contract (medium->class medium)
+  (-> (or/c or/c 'anime 'cartoon 'comic 'ln 'manga 'television) data-class?)
+  (case medium
+    [(anime) anime%]
+    [(cartoon) cartoon%]
+    [(comic) comic%]
+    [(ln) ln%]
+    [(manga) manga%]
+    [(television) television%]))
+
+(define/contract (db-has-key? #:db-conn [db-conn sqlc] medium title)
+  (->* ([or/c 'anime 'cartoon 'comic 'ln 'manga 'television] string?)
+       (#:db-conn connection?)
+       boolean?)
+  (define mc (medium->class medium))
+  (define objs (select-data-objects db-conn mc (where (= title ?)) title))
+  (not (null? objs)))
+
+(define/contract (table-titles #:db-conn [db-conn sqlc] medium)
+  (->* ([or/c 'anime 'cartoon 'comic 'ln 'manga 'television])
+       (#:db-conn connection?)
+       list?)
+  (define mc (medium->class medium))
+  (for/list ([db-obj (select-data-objects db-conn mc)])
+    (get-column title db-obj)))
+
+; error if title does not exist in medium
+(define/contract (get-rating #:db-conn [db-conn sqlc] medium title)
+  (->* ([or/c 'anime 'cartoon 'comic 'ln 'manga 'television] string?)
+       (#:db-conn connection?)
+       integer?)
+  (define mc (medium->class medium))
+  (define db-obj
+    (cond [(db-has-key? #:db-conn db-conn medium title)
+           (make-data-object db-conn mc title)]
+          [else #f]))
+  (if db-obj
+      (get-column rating db-obj)
+      (raise-arguments-error 'get-rating "nonexistent title in medium"
+                             "Title" title
+                             "Medium" medium)))
+
+(define/contract (get-date-start #:db-conn [db-conn sqlc] medium title)
+  (->* ([or/c 'anime 'cartoon 'comic 'ln 'manga 'television] string?)
+       (#:db-conn connection?)
+       string?)
+  (define mc (medium->class medium))
+  (define db-obj
+    (cond [(db-has-key? #:db-conn db-conn medium title)
+           (make-data-object db-conn mc title)]
+          [else #f]))
+  (if db-obj
+      (get-column date-start db-obj)
+      (raise-arguments-error 'get-rating "nonexistent title in medium"
+                             "Title" title
+                             "Medium" medium)))
+
+(define/contract (get-progress #:db-conn [db-conn sqlc] medium title)
+  (->* ([or/c 'anime 'cartoon 'comic 'ln 'manga 'television] string?)
+       (#:db-conn connection?)
+       string?)
+  (define mc (medium->class medium))
+  (define db-obj
+    (cond [(db-has-key? #:db-conn db-conn medium title)
+           (make-data-object db-conn mc title)]
+          [else #f]))
+  (if db-obj
+      (get-column progress db-obj)
+      (raise-arguments-error 'get-rating "nonexistent title in medium"
+                             "Title" title
+                             "Medium" medium)))
+
+(define/contract (get-date-end #:db-conn [db-conn sqlc] medium title)
+  (->* ([or/c 'anime 'cartoon 'comic 'ln 'manga 'television] string?)
+       (#:db-conn connection?)
+       string?)
+  (define mc (medium->class medium))
+  (define db-obj
+    (cond [(db-has-key? #:db-conn db-conn medium title)
+           (make-data-object db-conn mc title)]
+          [else #f]))
+  (if db-obj
+      (get-column date-end db-obj)
+      (raise-arguments-error 'get-rating "nonexistent title in medium"
+                             "Title" title
+                             "Medium" medium)))
+
+(define/contract (get-comments #:db-conn [db-conn sqlc] medium title)
+  (->* ([or/c 'anime 'cartoon 'comic 'ln 'manga 'television] string?)
+       (#:db-conn connection?)
+       string?)
+  (define mc (medium->class medium))
+  (define db-obj
+    (cond [(db-has-key? #:db-conn db-conn medium title)
+           (make-data-object db-conn mc title)]
+          [else #f]))
+  (if db-obj
+      (get-column comments db-obj)
+      (raise-arguments-error 'get-rating "nonexistent title in medium"
+                             "Title" title
+                             "Medium" medium)))
+
+; create a new entry in the correct table
+; if the entry already exists, don't do much of anything
+(define/contract (add-title! #:db-conn [db-conn sqlc] medium title)
+  (->* ([or/c 'anime 'cartoon 'comic 'ln 'manga 'television] string?)
+       (#:db-conn connection?)
+       void?)
+  (define mc (medium->class medium))
+  (define db-obj
+    (cond [(db-has-key? #:db-conn db-conn medium title)
+           (make-data-object db-conn mc title)]
+          [else (new mc [title title])]))
+  (save-data-object db-conn db-obj))
+
+(define/contract (del-title! #:db-conn [db-conn sqlc] medium title)
+  (->* ([or/c 'anime 'cartoon 'comic 'ln 'manga 'television] string?)
+       (#:db-conn connection?)
+       void?)
+  (when (db-has-key? #:db-conn sqlc medium title)
+    (define db-obj (make-data-object db-conn (medium->class medium) title))
+    (delete-data-object sqlc db-obj)))
+
+(define/contract (set-info! #:db-conn [db-conn sqlc] medium title r ds p de c)
+  (->* ([or/c 'anime 'cartoon 'comic 'ln 'manga 'television]
+        string?
+        integer?
+        string?
+        string?
+        string?
+        string?)
+       (#:db-conn connection?)
+       void?)
+  (define mc (medium->class medium))
+  (define db-obj
+    (cond [(db-has-key? #:db-conn db-conn medium title)
+           (make-data-object db-conn mc title)]
+          [else
+           (new mc [title title])]))
+  (set-column! rating db-obj r)
+  (set-column! date-start db-obj ds)
+  (set-column! progress db-obj p)
+  (set-column! date-end db-obj de)
+  (set-column! comments db-obj c)
+  (save-data-object db-conn db-obj))
--- frame.rkt
+++ frame.rkt
@@ -4,6 +4,7 @@
          framework
          racket/class
          racket/gui/base
+         srfi/13
          "base.rkt"
          "db.rkt")
 (provide sr-frame)
@@ -14,8 +15,17 @@
 (void (exit:insert-on-callback (λ () (disconnect sqlc))))
 ; end framework
 
+(define medium-box (box 'anime))
+
+(define closer-frame%
+  (class frame%
+    (super-new)
+    ; clean up and exit the program
+    (define (on-close) (exit:exit))
+    (augment on-close)))
+
 (define sr-frame
-  (new frame%
+  (new closer-frame%
        [label "sr"]
        [width 500]
        [height 600]))
@@ -46,18 +56,6 @@
        [parent menu-bar]
        [label "&Edit"]))
 
-(define menu-bar-edit-add
-  (new menu-item%
-       [parent menu-bar-edit]
-       [label "&Add Title"]
-       [callback (λ (b evt) (void))]))
-
-(define menu-bar-edit-del
-  (new menu-item%
-       [parent menu-bar-edit]
-       [label "&Delete Title"]
-       [callback (λ (b evt) (void))]))
-
 (define menu-bar-edit-stats
   (new menu-item%
        [parent menu-bar-edit]
@@ -73,31 +71,118 @@
        [alignment '(left center)]
        [stretchable-height #f]))
 
-(define medium
+(define medium-choice
   (new choice%
        [parent header-hpanel]
        [label "Medium"]
        [choices '("Anime"
-                  "Light Novel"
+                  "Cartoon"
+                  "Comic"
+                  "LN"
                   "Manga"
-                  "Visual Novel")]
+                  "Television")]
        [callback (λ (choice evt)
                    ; modify title-lbox based on selection
-                   (void))]))
+                   (send title-lbox clear)
+                   (set-box! medium-box
+                             (string->symbol
+                              (string-downcase
+                               (send choice get-string-selection))))
+                   (for ([title (in-list (table-titles (unbox medium-box)))])
+                     (send title-lbox append title))
+                   ; set info side to defaults
+                   (set-info-defaults!))]))
 
+; begin title dialog box
+(define title-dialog
+  (new dialog%
+       [label "sr - Add Title"]
+       [width 400]
+       [height 100]))
+
+(define title-hpanel
+  (new horizontal-panel% [parent title-dialog]))
+
+(define title-tfield
+  (new text-field%
+       [parent title-hpanel]
+       [label ""]
+       [callback (λ (tfield evt)
+                   (when (eq? (send evt get-event-type) 'text-field-enter)
+                     (define title-text (send title-tfield get-value))
+                     ; disallow the empty string
+                     (unless (string-null? title-text)
+                       (send title-lbox append title-text)
+                       (send tfield set-value "")
+                       (send title-dialog show #f))))]))
+
+; Ok/Cancel order based on system
+(cond [(system-position-ok-before-cancel?)
+       ; ok-button
+       (new button%
+            [parent title-hpanel]
+            [label "Ok"]
+            [callback (λ (button evt)
+                        (define title-text (send title-tfield get-value))
+                        ; disallow the empty string
+                        (unless (string-null? title-text)
+                          (send title-lbox append title-text)
+                          (send title-tfield set-value "")
+                          (send title-dialog show #f)))])
+       ; cancel-button
+       (new button%
+            [parent title-hpanel]
+            [label "Cancel"]
+            [callback (λ (button evt)
+                        (send title-tfield set-value "")
+                        (send title-dialog show #f))])
+       (void)]
+      [else
+       ; cancel-button
+       (new button%
+            [parent title-hpanel]
+            [label "Cancel"]
+            [callback (λ (button evt)
+                        (send title-tfield set-value "")
+                        (send title-dialog show #f))])
+       ; ok-button
+       (new button%
+            [parent title-hpanel]
+            [label "Ok"]
+            [callback (λ (button evt)
+                        (define title-text (send title-tfield get-value))
+                        ; disallow the empty string
+                        (unless (string-null? title-text)
+                          (send title-lbox append title-text)
+                          (send title-tfield set-value "")
+                          (send title-dialog show #f)))])
+       (void)])
+; end title dialog box
+
+; add to bottom of the list and wait until user clicks "Save" button
 (define add-title
   (new button%
        [parent header-hpanel]
        [label "+"]
        [callback (λ (button evt)
-                   (void))]))
+                   ; ask the user to enter the title of the series
+                   (send title-dialog show #t))]))
 
+; immediately
 (define del-title
   (new button%
        [parent header-hpanel]
        [label "-"]
        [callback (λ (button evt)
-                   (void))]))
+                   (define title (send title-lbox get-string-selection))
+                   ; don't do anything if there isn't anything selected
+                   (when title
+                     ; remove from lbox
+                     (send title-lbox delete (send title-lbox get-selection))
+                     ; don't do db stuff on an empty string
+                     (unless (string-null? title)
+                       ; remove from database
+                       (del-title! (unbox medium-box) title))))]))
 ; end header content
 
 ; begin body content
@@ -109,11 +194,29 @@
        [label ""]
        [parent body-hpanel]
        [style '(single)]
-       [choices (list "")]
+       ; if there are any anime entries then use those
+       ; otherwise use default empty string
+       [choices (let ([default (list "")]
+                      [anime-lst (table-titles 'anime)])
+                  (if (null? anime-lst)
+                      default
+                      anime-lst))]
        [callback (λ (lbox evt)
-                   ; clear out all fields in the right side
-                   ; and fill them with database entries
-                   (void))]))
+                   (when (eq? (send evt get-event-type) 'list-box)
+                     (define title (send lbox get-string-selection))
+                     ; clear out all fields in the right side
+                     (set-info-defaults!)
+                     ; and fill them with database entries
+                     (let ([rating (get-rating (unbox medium-box) title)]
+                           [date-start (get-date-start (unbox medium-box) title)]
+                           [progress (get-progress (unbox medium-box) title)]
+                           [date-end (get-date-end (unbox medium-box) title)]
+                           [comments (get-comments (unbox medium-box) title)])
+                       (send rating-choice set-string-selection (number->string rating))
+                       (send date-start-tfield set-value date-start)
+                       (send progress-tfield set-value progress)
+                       (send date-end-tfield set-value date-end)
+                       (send comments-tfield set-value comments))))]))
 
 ; begin info section
 (define info-vpanel
@@ -121,27 +224,27 @@
        [parent body-hpanel]
        [alignment '(left center)]))
 
-(define rating
+(define rating-choice
   (new choice%
        [parent info-vpanel]
        [label "Rating"]
        [choices (for/list ([i (in-range 5 0 -1)]) (number->string i))]
-       ; select string "3"
+       ; select string "3" as default
        [selection 2]))
 
-(define date-started
+(define date-start-tfield
   (new text-field%
        [parent info-vpanel]
        [label "Date Started"]
        [style '(single vertical-label)]))
 
-(define progress
+(define progress-tfield
   (new text-field%
        [parent info-vpanel]
        [label "Progress"]
        [style '(single vertical-label)]))
 
-(define completed/dropped
+(define date-end-tfield
   (new text-field%
        [parent info-vpanel]
        [label "Date Completed/Dropped"]
@@ -153,6 +256,78 @@
        [label "Comments"]
        [style '(multiple vertical-label)]))
 
-; Ok/Clear buttons ordered by host system
+; set all the info fields to defaults
+(define (set-info-defaults!)
+  (send rating-choice set-selection 2)
+  (send date-start-tfield set-value "")
+  (send progress-tfield set-value "")
+  (send date-end-tfield set-value "")
+  (send comments-tfield set-value ""))
+
+(define button-hpanel
+  (new horizontal-panel%
+       [parent info-vpanel]
+       [alignment '(right center)]
+       [stretchable-height #f]))
+
+; Save/Clear buttons ordered by host system
+(cond [(system-position-ok-before-cancel?)
+       (new button%
+            [parent button-hpanel]
+            [label "Save"]
+            [callback (λ (button evt)
+                        ; if the title is an empty string or
+                        ; there's no title selected, do nothing
+                        (define title (send title-lbox get-string-selection))
+                        (when (and title (not (string-null? title)))
+                          (define rating
+                            (string->number
+                             (send rating-choice get-string-selection)))
+                          (define date-start (send date-start-tfield get-value))
+                          (define progress (send progress-tfield get-value))
+                          (define date-end (send date-end-tfield get-value))
+                          (define comments (send comments-tfield get-value))
+                          (set-info! (unbox medium-box)
+                                     title
+                                     rating
+                                     date-start
+                                     progress
+                                     date-end
+                                     comments)))])
+       (new button%
+            [parent button-hpanel]
+            [label "Clear"]
+            [callback (λ (button evt)
+                        (set-info-defaults!))])
+       (void)]
+      [else
+       (new button%
+            [parent button-hpanel]
+            [label "Clear"]
+            [callback (λ (button evt)
+                        (set-info-defaults!))])
+       (new button%
+            [parent button-hpanel]
+            [label "Save"]
+            [callback (λ (button evt)
+                        ; if the title is an empty string or
+                        ; there's no title selected, do nothing
+                        (define title (send title-lbox get-string-selection))
+                        (when (and title (not (string-null? title)))
+                          (define rating
+                            (string->number
+                             (send rating-choice get-string-selection)))
+                          (define date-start (send date-start-tfield get-value))
+                          (define progress (send progress-tfield get-value))
+                          (define date-end (send date-end-tfield get-value))
+                          (define comments (send comments-tfield get-value))
+                          (set-info! (unbox medium-box)
+                                     title
+                                     rating
+                                     date-start
+                                     progress
+                                     date-end
+                                     comments)))])
+       (void)])
 ; end info section
 ; end body content
